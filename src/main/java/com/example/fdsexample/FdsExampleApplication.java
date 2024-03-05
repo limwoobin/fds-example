20edcd85d144c58530e7e91f22586901553a6750
woobeen-lim <woobeen.lim@myrealtrip.com>
2024-03-05 19:31:11 +0900

    feat: fds study init

---
+++ src/main/java/com/example/fdsexample/FdsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.fdsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdsExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FdsExampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/fdsexample/FdsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fdsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdsExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
